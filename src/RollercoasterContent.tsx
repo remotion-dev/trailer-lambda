fdb9c519dfa4983ddabc61244192ab15b346ec65
Jonny Burger <jonathanburger11@gmail.com>
2021-07-14 23:05:27 +0200

    slow but ok

--- src/RollercoasterContent.tsx
+++ src/RollercoasterContent.tsx
@@ -1,6 +1,6 @@
-import {Environment} from 'drei';
+import {Environment} from '@react-three/drei';
+import {useThree} from '@react-three/fiber';
 import React, {useEffect} from 'react';
-import {useThree} from 'react-three-fiber';
 import {interpolate, useCurrentFrame} from 'remotion';
 import SimplexNoise from 'simplex-noise';
 import {Curve, Vector3} from 'three';
@@ -8,23 +8,52 @@ import {CAMERA_POSITION} from './rollercoaster-const';
 
 const simplexNoise = new SimplexNoise('seed');
 
-const getPointFor = (prog: number) => {
-	const tx = simplexNoise.noise2D(0, prog) * 100;
-	const ty = Math.sin(2 * Math.PI * prog * 2) * 10;
-	const tz = interpolate(prog, [0, 1], [200, 0]);
+const getPointFor = (prog: number, show: number) => {
+	const actualProg = Math.min(prog, show);
+	const tx = simplexNoise.noise2D(0, actualProg * 2) * 100;
+	const ty = Math.sin(2 * Math.PI * actualProg * 2) * 10;
+	const tz = interpolate(actualProg, [0, 1], [1000, 0]);
 
 	return [tx, ty, tz];
 };
 
+const getPointForOut = (prog: number, show: number) => {
+	const actualProg = Math.min(prog, show);
+
+	const [tx, ty, tz] = getPointFor(actualProg, show);
+	const offset = simplexNoise.noise2D(2, actualProg * 2) * 2;
+	return [
+		tx + Math.sin(actualProg * 50) * (offset + 5),
+		ty + Math.cos(actualProg * 50) * (offset + 5),
+		tz,
+	];
+};
+
 class CustomSinCurve extends Curve<Vector3> {
+	max: number;
+	constructor(max: number) {
+		super();
+		this.max = max;
+	}
 	getPoint(prog: number) {
-		const [tx, ty, tz] = getPointFor(prog);
+		const [tx, ty, tz] = getPointFor(prog, this.max);
 
 		return new Vector3().set(tx, ty, tz);
 	}
 }
 
-const path = new CustomSinCurve();
+class Outside extends Curve<Vector3> {
+	max: number;
+	constructor(max: number) {
+		super();
+		this.max = max;
+	}
+	getPoint(prog: number) {
+		const [tx, ty, tz] = getPointForOut(prog, this.max);
+
+		return new Vector3().set(tx, ty, tz);
+	}
+}
 
 const delta = 0.001;
 
@@ -32,15 +61,17 @@ export const RollercoasterContent: React.FC = () => {
 	const three = useThree();
 
 	const frame = useCurrentFrame();
-	const alongLine = interpolate(frame, [0, 700], [0, 1 - delta], {
+	const alongLine = interpolate(frame, [0, 250], [0, 1 - delta], {
 		extrapolateLeft: 'clamp',
 		extrapolateRight: 'clamp',
 	});
 
+	const path = new CustomSinCurve(alongLine + 0.05);
+	const path2 = new Outside(alongLine + 0.05);
+
 	useEffect(() => {
 		const point = path.getPointAt(alongLine);
 		const tangent = path.getTangentAt(alongLine);
-
 		three.camera.position.set(point.x, point.y + 10, point.z);
 
 		three.camera.lookAt(
@@ -48,7 +79,7 @@ export const RollercoasterContent: React.FC = () => {
 			point.y + 10 + tangent.y,
 			point.z + tangent.z
 		);
-	}, [alongLine, three.camera]);
+	}, [alongLine, path, three.camera]);
 
 	return (
 		<>
@@ -63,8 +94,12 @@ export const RollercoasterContent: React.FC = () => {
 				position={CAMERA_POSITION}
 			/>
 			<mesh receiveShadow>
-				<tubeGeometry attach="geometry" args={[path, 100, 2, 100, false]} />
-				<meshStandardMaterial transparent attach="material" color="#9c88ff" />
+				<tubeGeometry attach="geometry" args={[path, 400, 2, 100, false]} />
+				<meshStandardMaterial transparent attach="material" color="#e9f5ff" />
+			</mesh>
+			<mesh receiveShadow>
+				<tubeGeometry attach="geometry" args={[path2, 4000, 0.3, 400, false]} />
+				<meshStandardMaterial transparent attach="material" color="#6bbfff" />
 			</mesh>
 		</>
 	);
