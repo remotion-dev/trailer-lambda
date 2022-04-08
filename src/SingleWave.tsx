421caf68b7c834e774331ab2011a0d60b86b35d0
Jonny Burger <jonathanburger11@gmail.com>
2022-04-08 14:45:08 +0200

    speedup

--- src/SingleWave.tsx
+++ src/SingleWave.tsx
@@ -6,13 +6,11 @@ import {
 	useCurrentFrame,
 	useVideoConfig,
 } from 'remotion';
-import {makeSvgWave, svgPathToD} from './make-svg-wave';
 
 export const Wave: React.FC<{
 	color: string;
-	topOffset: number;
-}> = ({color, topOffset}) => {
-	const {height, width, fps} = useVideoConfig();
+}> = ({color}) => {
+	const {fps} = useVideoConfig();
 	const frame = useCurrentFrame();
 
 	const progress = spring({
@@ -20,29 +18,28 @@ export const Wave: React.FC<{
 		frame,
 		config: {
 			damping: 200,
-			mass: 100,
+			mass: 20,
 		},
 	});
 
-	const amplitude = interpolate(progress, [0, 1], [0, 0]);
 	const radius = interpolate(progress, [0, 1], [0, 7000]);
 
-	const rotation = interpolate(progress, [0, 1], [0, Math.PI * 0.05]);
-
-	const path = makeSvgWave({
-		height,
-		numberOfCurves: 50,
-		amplitude,
-		radius,
-		width,
-		topOffset,
-		rotation,
-	});
 	return (
-		<AbsoluteFill>
-			<svg width={width} height={height} viewBox={`0 0 ${width} ${height}`}>
-				<path d={svgPathToD(path)} fill={color} strokeWidth={10} />
-			</svg>
+		<AbsoluteFill
+			style={{
+				justifyContent: 'center',
+				alignItems: 'center',
+			}}
+		>
+			<div
+				style={{
+					height: radius,
+					width: radius,
+					position: 'absolute',
+					backgroundColor: color,
+					borderRadius: radius / 2,
+				}}
+			/>
 		</AbsoluteFill>
 	);
 };
--- src/Wave.tsx
+++ src/Wave.tsx
@@ -5,16 +5,12 @@ import {NUMBER_OF_WAVES, WAVES_START, WAVE_EVERY_FRAMES} from './math/waves';
 import {Wave} from './SingleWave';
 
 export const ManyWaves: React.FC = () => {
-	const globaltopOffset = 0;
 	return (
 		<AbsoluteFill>
 			{new Array(NUMBER_OF_WAVES).fill(true).map((_, index) => {
 				return (
 					<Sequence from={index * WAVE_EVERY_FRAMES + WAVES_START}>
-						<Wave
-							color={index % 2 === 0 ? COLORS[0] : 'white'}
-							topOffset={globaltopOffset}
-						/>
+						<Wave color={index % 2 === 0 ? COLORS[0] : 'white'} />
 					</Sequence>
 				);
 			})}
